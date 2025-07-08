6c9a79829e949efd38246d0ddb850a061100c242
NEGRIC214 <noelia.espillaque12@gmail.com>
2025-07-08 18:19:50 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+    function infodisco(NombreArtista, NombreDisco, NumeroCanciones, MillonesVendidosDiscos){
+       alert("Artista: "+ NombreArtista + "\n" +
+        "Nombre del Disco:" + NombreDisco + "\n" +
+         "Numero de Canciones:"+ NumeroCanciones + "\n" +
+          "Millones de Discos Vendidos:" + MillonesVendidosDiscos);
+    }
+
+
+    /*function seleccionarProducto( tposter, tnombre, tprecio) {
+      const url = new URL("detalles.html", window.location.href);
+      url.searchParams.set("poster", tposter);
+      url.searchParams.set("nombre", tnombre);
+      url.searchParams.set("precio", tprecio);
+      window.location.href = url.toString();
+    }*/
